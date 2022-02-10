3ed90abee8b0e4050ef7f311f062eeaeaf020147
Flavio <flavio.dediego@gmail.com>
2022-02-10 18:49:52 +0100

    Subiendo test

--- src/test/java/com/example/demo/BuenoSpringApplicationTests.java
+++ src/test/java/com/example/demo/BuenoSpringApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.Pedido;
+import com.example.demo.service.PedidoService;
 
 @SpringBootTest
 class BuenoSpringApplicationTests {
 
+	@Autowired
+	private PedidoService pedidoService;
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void prueba() {
+		Pedido pedido = new Pedido();
+		pedidoService.addPedido(1, pedido, 1);
+		assertEquals(20.0, pedidoService.calculaPrecioTotal(pedido));
+		
 	}
 
 }
